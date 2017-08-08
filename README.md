4aa2070d1ea4f3377ff25f5c1d1b50cab96f0827
lizz <363664470@qq.com>
2017-08-08 22:34:43 +0800

    good.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # PrintLog
+
+An incredibly simple logger!
+All it does is to log all outputs of `print` and `println` into a file.
+
+Usage:
+```julia
+julia> using PrintLog
+
+julia> @printlog "bf.log"
+INFO: `print` and `println` will be logged into file `bf.log`
+
+julia> println("bufan")
+bufan
+
+julia> @noprintlog
+INFO: `print` and `println` are resumed.
+
+julia> println("fanfan")
+fanfan
+```
+And here is what `bf.log` looks like:
+```txt
+bufan
+```
+
+---
+Created by Zhizhong Li.
--- src/PrintLog.jl
+++ src/PrintLog.jl
@@ -1,5 +1,53 @@
 module PrintLog
 
-# package code goes here
+import Suppressor.@suppress
+
+export @printlog, @noprintlog
+
+"""
+    @printlog "file.log"
+
+Enable `printlog`.
+Outputs of `print` and `println` will be logged into file `file.log`.
+It will create the log file if it does not exist.
+All prints will be appended to the end of the file.
+
+    @printlog "file.log" silent
+
+Silent mode.
+Do not output `info`.
+"""
+macro printlog(file, silent=false)
+    @eval begin
+        import Base.println, Base.print, Suppressor.@suppress
+        @suppress Base.println(xs...) =
+            open(f -> (println(f, xs...); println(STDOUT, xs...)), $file, "a+")
+        @suppress Base.print(xs...) =
+            open(f -> (print(f, xs...); print(STDOUT, xs...)), $file, "a+")
+    end
+    silent != :silent &&
+        info("`print` and `println` will be logged into file `$file`")
+    nothing
+end
+
+"""
+    @noprintlog
+
+Disable `printlog`.
+
+    @noprintlog silent
+
+Silent mode.
+"""
+macro noprintlog(silent=false)
+    @eval begin
+        import Base.println, Base.print, Suppressor.@suppress
+        @suppress Base.println(xs...) = println(STDOUT, xs...)
+        @suppress Base.print(xs...) = print(STDOUT, xs...)
+    end
+    silent != :silent &&
+        info("`print` and `println` are resumed.")
+    nothing
+end
 
 end # module
